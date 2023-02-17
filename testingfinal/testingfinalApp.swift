8b7de408e4b26fbab78d98b5cfcf77790f499777
kvscomputing <kvseetharaman2@gmail.com>
2023-02-17 03:08:00 -0800

    Initial Commit

---
+++ testingfinal/testingfinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingfinalApp.swift
+//  testingfinal
+//
+//  Created by Karthik Seetharaman on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct testingfinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
